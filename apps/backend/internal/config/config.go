ce0b5f4fe5d86e3c26572437b0c5f226c6e3a82b
shubh <shubhamwagh2544@gmail.com>
2025-12-29 11:08:46 +0530

    config loaded

--- apps/backend/internal/config/config.go
+++ apps/backend/internal/config/config.go
@@ -1,5 +1,16 @@
 package config
 
+import (
+	"os"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/v2"
+	"github.com/rs/zerolog"
+)
+
 type Config struct {
 	Primary  Primary        `koanf:"primary" validate:"required"`
 	Database DatabaseConfig `koanf:"database" validate:"required"`
@@ -42,5 +53,31 @@ type AuthConfig struct {
 }
 
 func LoadConfig() (*Config, error) {
-	return nil, nil
+	logger := zerolog.New(zerolog.ConsoleWriter{
+		Out: os.Stderr,
+	}).With().Timestamp().Logger()
+
+	k := koanf.New(".")
+
+	err := k.Load(env.Provider("BOILERPLATE_", ".", func(s string) string {
+		return strings.ToLower(strings.TrimPrefix(s, "BOILERPLATE_"))
+	}), nil)
+
+	if err != nil {
+		logger.Fatal().Err(err).Msg("could not load env file")
+	}
+
+	mainConfig := &Config{}
+	err = k.Unmarshal("", mainConfig)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("could not unmarshall main config")
+	}
+
+	validate := validator.New()
+	err = validate.Struct(mainConfig)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("config validation failed")
+	}
+
+	return mainConfig, nil
 }
